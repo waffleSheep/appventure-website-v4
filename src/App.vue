c8982aa2c646bf74c71c8129c88cd19b3b4cf81a
l-yc <22719973+l-yc@users.noreply.github.com>
2021-04-22 21:53:48 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
+  <div id="nav" class="medium-container">
+    <router-link to="/">AppVenture <span class="tiny-text">by NUS High</span></router-link>
+    <span style="flex: 1" />
     <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/cybersec">Cybersec</router-link>
+    <router-link to="/blog">Blog</router-link>
+    <router-link to="/contact">Contact</router-link>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '@/styles/main.scss';
+
+main {
+  @extend .medium-container;
 }
 
 #nav {
   padding: 30px;
 
+  display: flex;
+  flex-flow: row wrap;
+
   a {
     font-weight: bold;
     color: #2c3e50;
 
+    vertical-align: baseline;
+    padding: .2rem .6rem;
+    margin: 0 .8rem;
+    text-decoration: none;
+    border-radius: .2rem;
+
     &.router-link-exact-active {
-      color: #42b983;
+      color: #009a90;
+    }
+
+    &:hover {
+      background-color: lightgrey;
     }
   }
+}
+
+.tiny-text {
+  font-size: .6rem;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <main class="home">
+    <div class="banner medium-container content-section">
+      <div class="info">
+        <h3>An adventure into the work of our CS students</h3>
+
+        <a class="icon-button lab la-instagram" href="https://www.instagram.com/appventure_nush/"></a>
+        <a class="icon-button lab la-youtube" href="https://www.youtube.com/channel/UCBLSYkLt1hFDL7RL8rEeU0w/"></a>
+
+      </div>
+      <img alt="AppVenture logo" src="../assets/nush_animation.svg" height="256">
+    </div>
+  </main>
 </template>
+
+<style lang="scss">
+@import '@/styles/main.scss';
+
+.banner {
+  border-radius: 1rem;
+  background-color: $primary-color;
+  color: #fff;
+
+  padding: 1rem 2rem;
+
+  display: flex;
+  flex-flow: row wrap;
+
+  .info {
+    flex: 1;
+
+    h3 { color: #fff; }
+  }
+}
+
+a.icon-button {
+  text-decoration: none;
+  font-size: 2rem;
+
+  border: .2rem solid #fff;
+  border-radius: 50%;
+
+  padding: .2rem;
+
+  margin-right: .8rem;
+
+  background-color: #fff;
+  color: $primary-color;
+  transition: all ease-in-out .2s;
+
+  &:hover {
+    background-color: $primary-color;
+    color: #fff;
+  }
+}
+
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
   }
 })
 </script>
