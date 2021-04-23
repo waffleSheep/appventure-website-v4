4f7f044173592bfa9baf4b0239119e5bb98415de
l-yc <22719973+l-yc@users.noreply.github.com>
2021-04-23 11:24:20 +0800

    add credits

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,6 +14,12 @@
         <Profile v-for="p in profiles" v-bind:key="p.id" v-bind="p" />
       </div>
     </div>
+
+    <h1>Credits</h1>
+    <p>The first version of this site only lives on in vague Github descriptions.</p>
+    <p>The earliest recorded version of this site was hand-crafted by <a href="https://ambrose.makerforce.io/">Ambrose</a> and Wayne.</p>
+    <p>The next iteration was a rewrite in javascript by Xavier.</p>
+    <p>This version, heavily based on earlier works, is designed and maintained by Yue Chen, _____ (TBC).</p>
   </main>
 </template>
 
