3517de21f64c8a167839c3f0b8fead6c3da7bf1b
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-10-20 19:26:49 +0800

    add Contact Page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,25 @@
 <template>
   <Layout>
     <main>
-      <div class="preamble medium-container">
+      <div class="preamble medium-container lede text-center">
         <h1 class="text-center">Contact</h1>
-        <p class="lede text-center">We want to hear your app ideas! If you have an idea you need help bringing into reality, fill in the form below and we will get back to you!</p>
+        <p>
+          Enquiries? Email us at
+          <a href="mailto:appventure@nushigh.edu.sg">appventure@nushigh.edu.sg</a>.
+        </p>
+        <p>
+          For admission and general school hotlines, visit our
+          <a href="http://www.nushigh.edu.sg/others/contact-us">school website</a>.
+        </p>
+
       </div>
+
       <hr/>
+      <h2>
+        Requests
+      </h2>
+      <p class="lede text-center">We want to hear your app ideas! If you have an idea you need help bringing into reality, fill in the form below and we will get back to you! (Limited to those within the school)</p>
+      <iframe width="100%" height= "480px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=cnEq1_jViUiahddCR1FZKj6EDIDhbU5EohvQH6Z2Dw1UNzIxQkJEWlVaQ0JXN0ExRTg2M1EwMVM5Si4u&embed=true" frameborder= "0" marginwidth= "0" marginheight= "0" style= "border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
     </main>
   </Layout>
 </template>
