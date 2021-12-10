b09b379ce9b7d36fbf4f2bd08a9ab7ebff1642fc
lyc <22719973+l-yc@users.noreply.github.com>
2021-12-10 13:21:35 +0800

    add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <main>
+      <div class="preamble medium-container lede text-center">
+        <h1 class="text-center">Oops!</h1>
+        <p>
+			The page cannot be found. Go back to the <g-link to="/">homepage</g-link>?
+        </p>
+      </div>
+    </main>
+  </Layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ErrorPage extends Vue {
+	public metaInfo() {
+		return {
+			title: 'Not Found',
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
+
