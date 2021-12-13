42fed76f92dca594b0d7e0649c0ff1fe6db2c035
lyc <22719973+l-yc@users.noreply.github.com>
2021-12-13 14:12:11 +0800

    add compact project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="medium-container project-card">
+  <div :class="'project-card ' + (fancy ? 'fancy' : 'compact')">
     <g-link
       class="link"
       :to="'/projects/' + project.id"
     />
 
-    <g-image class="thumbnail" v-if="project.thumbnail" :src="project.thumbnail" />
+    <g-image 
+			class="thumbnail" 
+			:src="project.thumbnail || 'https://via.placeholder.com/128'" />
     <div class="content">
       <h5 class="name">{{ project.name }}</h5>
-      <div class="created">
+      <div v-if="fancy" class="created">
         <span v-for="(c, idx) in project.created.contributors" :key="c.id">
           {{c.name}}<span v-if="idx < project.created.contributors.length-2">, </span>
           <span v-if="idx === project.created.contributors.length-2">, and </span>
         </span>
       </div>
-      <div class="tags">
-        <TagChip class="tagChip"
-                 v-for="tag in project.tags"
-                 :key="tag.id"
-                 :tag="tag"
-                 :filled="false"
-                 :link-enabled="false">
-        </TagChip>
+      <div v-if="fancy" class="tags">
+        <TagChip 
+          class="tagChip"
+          v-for="tag in project.tags"
+          :key="tag.id"
+          :tag="tag"
+          :filled="false"
+          :link-enabled="false" />
       </div>
     </div>
   </div>
@@ -41,20 +43,65 @@ import { Contribution } from '../types/Contribution';
 })
 export default class ProjectCard extends Vue {
   @Prop() project!: Project;
+  @Prop({ default: true }) fancy!: boolean;
   Contribution = Contribution
 }
 </script>
 
 <style lang="scss" scoped>
 
-div.project-card {
+.project-card {
   position: relative;
+
+  .link {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.project-card.compact:not(:first-child) {
+	margin-left: 32px;
+}
+
+.project-card.compact {
   display: flex;
   flex-flow: row wrap;
+	margin-bottom: 64px;
 
   border-radius: 1rem;
+  box-shadow: 0 5px 9px 2px rgba(0, 0, 0, 0.1);
+
+  background-color: white;
+  transition: all .2s;
+  &:hover {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 23px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  .thumbnail {
+    margin: 4px;
+    border-radius: 1rem;
+    border: 1px solid #eee;
+  }
+
+	.content {
+		position: absolute;
+		top: 100%;
+		width: 100%;
+		text-align: center;
+
+		margin-top: 8px;
+		h5 { margin: 0; }
+	}
+}
+
+.project-card.fancy {
+  display: flex;
+  flex-flow: row wrap;
   margin-bottom: 2rem;
 
+  border-radius: 1rem;
   padding: 4px;
   box-shadow: 0 5px 9px 2px rgba(0, 0, 0, 0.1);
 
@@ -80,16 +127,10 @@ div.project-card {
     }
   }
 
-  .link {
-    position: absolute;
-    width: 100%;
-    height: 100%;
+  .tags {
+    display: flex;
+    justify-content: flex-end;
   }
-}
-
-.tags {
-  display: flex;
-  justify-content: flex-end;
 }
 
 </style>
