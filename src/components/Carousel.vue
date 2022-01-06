d3892b6cc8dab189fff33a4296b94c84741eebe7
lyc <22719973+l-yc@users.noreply.github.com>
2022-01-06 20:48:11 +0800

    fix carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="medium-container carousel">
-    <div class="viewport">
+    <div ref="viewport" class="viewport" dir="ltr" @scroll="handleScroll">
       <g-image class="image"
-        v-if="activeImage"
-        :src="activeImage"
-        :key="activeImage.id"
-        @click="index=activeIdx"
-        fit="contain"
+        v-for="i in this.gallery"
+        :src="i"
+        :key="i.src"
       />
     </div>
-<!--    <div>-->
-<!--      <client-only>-->
-<!--      <VueGallery-->
-<!--        class="gallery"-->
-<!--        :images="images"-->
-<!--        :index="index"-->
-<!--        @close="index=0"-->
-<!--      />-->
-<!--      </client-only>-->
-<!--    </div>-->
+    <div class="viewport-indicator">
+      <div 
+        v-for="(i, idx) in this.gallery" 
+        :key="i.src"
+        :class="'light' + (idx == activeIdx ? ' active' : '')"
+        @click="setIndex(idx)"
+      >
+      </div>
+    </div>
 
-    <div class="navigation" v-if="gallery && gallery.length > 1">
-      <div class="prev button" @click="prev()">
+    <div class="navigation">
+      <div class="prev button" @click="prev()" v-if="gallery && activeIdx > 0">
         <chevron-left-icon size="1.5x" ></chevron-left-icon>
       </div>
-      <div class="next button" @click="next()">
+      <div class="next button" @click="next()" v-if="gallery && activeIdx < gallery.length-1">
         <chevron-right-icon size="1.5x"></chevron-right-icon>
       </div>
     </div>
@@ -34,54 +31,100 @@
 <script lang="ts">
 import { Component, Vue, Prop  } from 'vue-property-decorator';
 import { ChevronLeftIcon, ChevronRightIcon } from 'vue-feather-icons';
-// import VueGallery from 'vue-gallery';
 
 @Component({
-  // @ts-ignore
   components: { ChevronLeftIcon, ChevronRightIcon  },
 })
 export default class Carousel extends Vue {
-  @Prop() gallery!: object[];
-  // @ts-ignore
-  // images = this.gallery.map(it => it.src);
+  @Prop() gallery!: typeof Image[];
+  viewport?: HTMLElement;
   activeIdx = 0;
   index = null;
+  breakpoints: number[] = [];
 
-  get activeImage(): any {
-    if (!this.gallery) return null;
-    return this.gallery[this.activeIdx];
+  mounted() {
+    // @ts-ignore
+    this.viewport = this.$refs.viewport;
+    this.viewport?.querySelectorAll('img').forEach((e) => {
+      this.breakpoints.push(e.offsetLeft);
+    });
   }
 
   prev() {
-    this.activeIdx = this.activeIdx -1 === 0 ? this.gallery.length -1 : this.activeIdx-1;
+    this.setIndex(this.activeIdx-1 < 0 ? this.gallery.length-1 : this.activeIdx-1);
   }
 
   next() {
-    this.activeIdx = this.activeIdx+1 === this.gallery.length ? 0 : this.activeIdx + 1;
+    this.setIndex(this.activeIdx+1 === this.gallery.length ? 0 : this.activeIdx + 1);
+  }
+
+  setIndex(i: number) {
+    this.viewport?.querySelectorAll('img')[i].scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+      inline: 'nearest',
+    });
+  }
+
+  handleScroll() {
+    if (!this.viewport) return;
+    for (let i = this.breakpoints.length-1; i >= 0; --i) { // probably won't have many
+      if (this.viewport.scrollLeft >= this.breakpoints[i]-32) {
+        this.activeIdx = i;
+        break;
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 .carousel {
   position: relative;
   margin-top: 2rem;
-  margin-bottom: 2rem;
+  margin-bottom: 2rem; 
 
   .viewport {
     display: flex;
-    flex-flow: column nowrap;
+    flex-flow: row nowrap;
+    overflow: auto;
+    scroll-snap-type: x mandatory;
+
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    &::-webkit-scrollbar { display: none; } /* Chrome, Safari and Opera */
 
     .image {
       background-color: white;
       min-height: 5rem;
+      flex: none;
       width: 100%;
       margin: 2px;
       border-radius: 1rem;
       box-shadow: 1px 1px 1px 1px #eee;
+      scroll-snap-align: center;
     }
   }
+
+  .viewport-indicator {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    padding: 8px;
+
+    .light {
+      border-radius: 9999px;
+      width: 12px;
+      height: 12px;
+      margin: 0 2px;
+      background-color: #ccc;
+      cursor: pointer;
+    }
+
+    .light.active {
+      background-color: $primary-color;
+    }
+  } 
 
   .button {
     padding: 0;
@@ -97,8 +140,6 @@ export default class Carousel extends Vue {
     opacity: 0.3;
     transition: all 0.3s;
 
-
-
     &:hover {
       filter: saturate(1.0);
       opacity: 1.0;
@@ -108,18 +149,13 @@ export default class Carousel extends Vue {
   .prev {
     position: absolute;
     left: 0;
-    top: calc(50% - 1rem);
+    top: calc(50% - 1.5rem);
   }
 
   .next {
     position: absolute;
     right: 0;
-    top: calc(50% - 1rem);
-  }
-}
-.gallery::v-deep {
-  img.slide-content {
-    background-color: white;
+    top: calc(50% - 1.5rem);
   }
 }
 
